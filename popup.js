c09cd6a87eeedce559c97f021b49d4054f3cf6b1
Georgi Varbanov <gvarbanov@usf.edu>
2025-08-23 19:19:14 -0400

    Add some initial response display

--- popup.js
+++ popup.js
@@ -1,5 +1,24 @@
-chrome.storage.local.get(["analysisResult"]).then((result) => {
-  const analysis = result.analysisResult;
-  console.log("Value is:", analysis);
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const displayElement = document.getElementById('analysisParagraph');
+
+  chrome.storage.local.get(["analysisResult"]).then((result) => {
+    const analysis = result.analysisResult;
+    console.log("Value is:", analysis);
+
+    if (analysis){
+
+      const formattedHtml = `
+        <strong>Fallacy Detected:</strong> ${analysis.fallacy}<br>
+        <strong>Confidence:</strong> ${Math.round(analysis.confidence * 100)}<br>
+        <strong>Explanation:</strong> ${analysis.explanation}
+      `;
+      displayElement.textContent = formattedHtml;
+    } else {
+      const formattedHtml = `<strong>Analysis In Progress...</strong>`
+      displayElement.textContent = formattedHtml;
+    }
+  });
 });
+
 
