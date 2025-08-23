f0743221dbc509a965c6620a5da4d505d6f2eb06
Georgi Varbanov <gvarbanov@usf.edu>
2025-08-23 17:49:13 -0400

    Add skeleton function for getting model response

--- sw.js
+++ sw.js
@@ -5,8 +5,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const highlightedText = request.text;
     console.log("Received message from:", sender.tab ? sender.tab.url : "the extension");
     // Perform action here ...
-    console.log("highlighted text:", highlightedText)
+    console.log("highlighted text:", highlightedText);
+    model_response = getModelResponse(highlightedText)
 
     sendResponse({status: "success", message: "Action performed"});
   }
 });
+
+async function getModelResponse(text) {
+  const url = "need to add";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({highlighted_text: text})
+    });
+  if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+  const result = await response.json();
+  console.log(result);
+  return result;
+
+  } catch (error) {
+    console.error(error.message);
+  }
+  
+}
