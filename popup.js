475d81fce3ffb5ed9a0e416494f362918f64615b
Georgi Varbanov <gvarbanov@usf.edu>
2025-09-05 10:41:48 -0400

    formatting

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
   const displayElement = document.getElementById('analysisParagraph');
-  
+
   // ask for both status and analysisResult
   chrome.storage.local.get(['status', 'analysisResult'], (result) => {
     const analysis = result.analysisResult;
     const status = result.status
 
-    if (status === "loading"){
+    if (status === "loading") {
       displayElement.innerHTML = '<strong>Analysis In Progress...</strong>';
-    } else if (status === "error"){
-      displayElement.innerHTML= "<strong>Sorry, an error has occurred.</strong>";
-    } else if (status === 'complete' && analysis){
+    } else if (status === "error") {
+      displayElement.innerHTML = "<strong>Sorry, an error has occurred.</strong>";
+    } else if (status === 'complete' && analysis) {
       const formattedHtml = `
         <strong>Fallacy Detected:</strong> ${analysis.fallacy}<br>
         <strong>Confidence:</strong> ${Math.round(analysis.confidence * 100)}<br>
