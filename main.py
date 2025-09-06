6e27472b326e5ed9fb732c5ad70aa2fdacac8dab
Georgi Varbanov <gvarbanov@usf.edu>
2025-09-06 14:21:15 -0400

    Add model loading logic

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import pipeline
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
+model_cache = {}
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Loading model...")
+    model_cache["fallacy_detector"] = pipeline("text-classification", model="SamanthaStorm/fallacyfinder")
+    print("Model loaded...")
+    yield
+    model_cache.clear()
+
+app = FastAPI(lifespan=lifespan)
 class TextItem(BaseModel):
     highlighted_text: str
 
@@ -12,4 +23,6 @@ class TextItem(BaseModel):
 async def analyze_text(item: TextItem):
     recieved_text = item.highlighted_text
     print(f"Recieved highlighted text: {recieved_text}")
-    return {"text_recieved": recieved_text, "analysis": "todo"}
+    model = model_cache["fallacy_detector"]
+    result = model(item.highlighted_text)
+    return {"analysis": result}
